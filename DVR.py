a1b9319df7f8e9e77595c23a2160d6c06bd48590
Abdullah Ibne Hanif Arean <abdullaharean2613@gmail.com>
2023-03-19 03:06:41 +0600

    dvr changed to fix some bugs

--- DVR.py
+++ DVR.py
@@ -5,15 +5,15 @@ import threading
 from socket import *
 import math
 
-SUSPEND_AFTER_TIMEOUT = 3.0 #timeout has happened. This time is waited until bellman-ford is executed.
+SUSPEND_AFTER_TIMEOUT = 3.
 
 class Path:
-    def __init__(self, distance, nextHop):
-        self.distance = distance
-        self.nextHop = nextHop
+    def __init__(self, dis, nxthop):
+        self.dis = dis
+        self.nxthop = nxthop
 
-    def equals(self, path2):  #to check if two paths are equal
-        return self.distance == path2.distance and self.nextHop == path2.nextHop  #
+    def equals(self, path2):
+        return self.dis == path2.dis and self.nxthop == path2.nxthop  #
 
 
 class Neighbour:
@@ -23,212 +23,213 @@ class Neighbour:
         self.timeout = timeout
         self.paths = dict()
 
-r_ID = str()
-r_port = int()
-r_fileName = str()
-r_neighbours = dict()
-r_routes = dict()
+router_id = str()
+router_port = int()
+router_filename = str()
+router_neighbours = dict()
+router_routes = dict()
 
-lock = threading.Lock()
+lockthread = threading.Lock()
 
-def create_pkt(dest_id, sendLinkCost):
-    distanceVector = str(r_ID)  #distance vector packet e.g. A  B  4.5 (paths)
+def pkt_send_creation(dest_id, cost):
+    distvector = str(router_id)
 
-    if sendLinkCost:  #if you wanna send
-        distanceVector += ' ' + str(r_neighbours[dest_id].linkCost)  #link cost of that router
-    distanceVector += '\n'
+    if cost: 
+        distvector += ' ' + str(router_neighbours[dest_id].linkCost)
+    distvector += '\n'
 
-    for id, path in r_routes.items():
-        if path.nextHop == dest_id:  #split horizon
-            distanceVector += str(id) + " " + str(math.inf) + '\n'
+    for id, path in router_routes.items():
+        if path.nxthop == dest_id:
+            distvector += str(id) + " " + str(math.inf) + '\n'
         else:
-            distanceVector += str(id) + " " + str(path.distance) + '\n'  #neigbor d
+            distvector += str(id) + " " + str(path.dis) + '\n'
 
-    return bytes(distanceVector, 'utf-8')
+    return bytes(distvector, 'utf-8')
 
 
-def sendDV(sendLinkCost):
+def distancevectorshare(cost):
     sendSocket = socket(AF_INET, SOCK_DGRAM)
-    lock.acquire()
-    for id, neighbour in r_neighbours.items():  #senidng to every neigbor
-        sendSocket.sendto(create_pkt(id, sendLinkCost), ('localhost', neighbour.port))
-    lock.release()
+    lockthread.acquire()
+    for id, neighbour in router_neighbours.items():
+        sendSocket.sendto(pkt_send_creation(id, cost), ('localhost', neighbour.port))
+    lockthread.release()
     sendSocket.close()
 
-def printTable():
+def printdistancetable():
     string = '\t'
 
-    for id in sorted(r_routes.keys()):  #router IDs
+    for id in sorted(router_routes.keys()): 
         string += '\t' + id
     print(string)
 
-    string = r_ID + '\t'
-    for id in sorted(r_routes.keys()):  #distance (specific to paths)
-        string += '\t' + str("%.1f" % r_routes[id].distance)
+    string = router_id + '\t'
+    for id in sorted(router_routes.keys()):
+        string += '\t' + str("%.1f" % router_routes[id].dis)
     print(string)
 
-    for id in sorted(r_neighbours.keys()):  #neigbors ka distance vector
-        string = id+'\t'+str(r_neighbours[id].linkCost)
-        for key2 in sorted(r_neighbours[id].paths.keys()):
-            string += '\t' + str("%.1f" % r_neighbours[id].paths[key2].distance)
+    for id in sorted(router_neighbours.keys()):
+        string = id+'\t'+str(router_neighbours[id].linkCost)
+        for key2 in sorted(router_neighbours[id].paths.keys()):
+            string += '\t' + str("%.1f" % router_neighbours[id].paths[key2].dis)
         print(string)
     print('')
 
-def timeOutCheck():
+def checktimeout():
     while 1:
-        time.sleep(1)  #after every 1 second
-        for id, neighbour in r_neighbours.items():  #going through every neighbor
-            s = socket(AF_INET, SOCK_DGRAM)  #UDP socket
+        time.sleep(1)
+        for id, neighbour in router_neighbours.items():
+            s = socket(AF_INET, SOCK_DGRAM)
             try:
-                s.bind(('localhost', neighbour.port))  #neighbor port reserved
-                #router is dead:
-                s.close() #port closed
-                if r_neighbours[id].linkCost != math.inf:  #if timeout not catered before
-                    lock.acquire()
+                s.bind(('localhost', neighbour.port))
+                s.close()
+                if router_neighbours[id].linkCost != math.inf:
+                    lockthread.acquire()
 
-                    r_routes[id].distance = math.inf  #infinity
-                    neighbour.linkCost = math.inf  #infinty
-                    neighbour.timeout = time.time()  #current time
-                    for key2, item2 in r_routes.items():  #all paths throughout it set to infinity
-                        if item2.nextHop == id:
-                            item2.distance = math.inf
+                    router_routes[id].dis = math.inf
+                    neighbour.linkCost = math.inf 
+                    neighbour.timeout = time.time()
+                    for key2, item2 in router_routes.items():
+                        if item2.nxthop == id:
+                            item2.dis = math.inf
 
-                    lock.release()
-                    sendDV(False)  #updated distance vector
-                    threading.Timer(SUSPEND_AFTER_TIMEOUT, target=bellManFord).start()  #bellman ford executed after that time
+                    lockthread.release()
+                    distancevectorshare(False)
+                    threading.Timer(SUSPEND_AFTER_TIMEOUT, target=distancevectoralgorithm).start()
             except:
-                pass  #router is alive
+                pass
 
-def listen():
-    listenSocket = socket(AF_INET, SOCK_DGRAM)
-    listenSocket.bind(('localhost', r_port))
+def threadlisten():
+    socketlisten = socket(AF_INET, SOCK_DGRAM)
+    socketlisten.bind(('localhost', router_port))
     while 1:
-        message, socketAddress = listenSocket.recvfrom(2048)
+        message, socketAddress = socketlisten.recvfrom(2048)
 
         lines = str(message)[2:len(str(message))-1].split('\\n')
         firstLine = lines[0].split()
         source = firstLine[0]
 
-        r_neighbours[source].timeout = -1.0
+        router_neighbours[source].timeout = -1.0
 
         if len(firstLine) > 1:
-            r_neighbours[source].linkCost = float(firstLine[1])
-            r_neighbours[source].timeout = -1
+            router_neighbours[source].linkCost = float(firstLine[1])
+            router_neighbours[source].timeout = -1
 
-        lock.acquire()
+        lockthread.acquire()
         for i in range(1, len(lines)):
             if lines[i] == '':
                 continue
             tokens = lines[i].split()
             newPath = Path(float(tokens[1]),'direct')
-            if tokens[0] not in r_neighbours[source].paths:
+            if tokens[0] not in router_neighbours[source].paths:
                 newNode(tokens[0])
-            if not r_neighbours[source].paths[tokens[0]].equals(newPath):
-                r_neighbours[source].paths[tokens[0]] = newPath
-        threading.Thread(target=bellManFord).start()
-        lock.release()
+            if not router_neighbours[source].paths[tokens[0]].equals(newPath):
+                router_neighbours[source].paths[tokens[0]] = newPath
+        threading.Thread(target=distancevectoralgorithm).start()
+        lockthread.release()
 
 
-def newNode(name):  #new neighbors ki baat
-    global r_neighbours
-    p = Path(math.inf, 'direct')  #infifinty initialize
-    r_routes[name] = p
-    for id, neighbour in r_neighbours.items():
-        neighbour.paths[name] = p #inifinty
+def newNode(name):
+    global router_neighbours
+    p = Path(math.inf, 'direct')
+    router_routes[name] = p
+    for id, neighbour in router_neighbours.items():
+        neighbour.paths[name] = p
 
 
-def bellManFord():
-    global r_routes
+def distancevectoralgorithm():
+    global router_routes
 
     isChanged = False
 
-    lock.acquire()
-    for id, route in r_routes.items():
+    lockthread.acquire()
+    for id, route in router_routes.items():
         m_list = list()
-        if id == r_ID:  #0
+        if id == router_id:
             continue
-        if id in r_neighbours:
-            if time.time() > r_neighbours[id].timeout and time.time() < r_neighbours[id].timeout + SUSPEND_AFTER_TIMEOUT:
-                r_routes[id] = Path(math.inf, 'direct')
+        if id in router_neighbours:
+            if time.time() > router_neighbours[id].timeout and time.time() < router_neighbours[id].timeout + SUSPEND_AFTER_TIMEOUT:
+                router_routes[id] = Path(math.inf, 'direct')
                 continue
             else:
-                m_list.append(Path(r_neighbours[id].linkCost, 'direct'))
+                m_list.append(Path(router_neighbours[id].linkCost, 'direct'))
 
-        for id2, neighbour in r_neighbours.items():
-            p = Path(r_neighbours[id2].linkCost + neighbour.paths[id].distance, id2)
+        for id2, neighbour in router_neighbours.items():
+            p = Path(router_neighbours[id2].linkCost + neighbour.paths[id].dis, id2)
             m_list.append(p)
         m_list.append(p)
-        m = min(m_list, key = lambda x: x.distance)  #main line nigga
-        if not r_routes[id].equals(Path(m.distance,m.nextHop)):
-            r_routes[id] = Path(m.distance,m.nextHop)
+        m = min(m_list, key = lambda x: x.dis)
+        if not router_routes[id].equals(Path(m.dis,m.nxthop)):
+            router_routes[id] = Path(m.dis,m.nxthop)
             isChanged = True
 
-    lock.release()
+    lockthread.release()
     if isChanged:
-        sendDV(False)
+        distancevectorshare(False)
 
 
-def menu():
+def UserPrompt():
     option = 0
     while(1):
-        print('\n****I AM ROUTER ' + r_ID + '****\n')
+        print('\n****ROUTER ' + router_id + '****\n')
 
-        option = int(input('1: Display Costs.\n2: Display distance vector table.\n3: Edit link costs\n4: Quit\nYour choice: '))
+        option = int(input('1: Display Costs of Reaching Other Routers.\n2: Display Distance Vector Table of The Router.\n3: Edit Cost of Link with Neighbour.\n4: Exit\nPlease Enter Your Choice:(1/2/3/4): '))
         if option == 1:
             print('Destination\tNext Hop\tDistance')
-            for id, route in sorted(r_routes.items()):
-                if id != r_ID:
-                    print('     ' + id + '\t\t' + route.nextHop + '\t\t' + str("%.1f" % route.distance))
+            for id, route in sorted(router_routes.items()):
+                if id != router_id:
+                    print('     ' + id + '\t\t' + route.nxthop + '\t\t' + str("%.1f" % route.dis))
         elif option == 2:
-            printTable()
+            printdistancetable()
         elif option == 3:
             string = 'Neighbours:'
-            for id in sorted(r_neighbours.keys()):
+            for id in sorted(router_neighbours.keys()):
                 string += ' ' + id
             print(string)
             toEdit = input('Enter which link to edit: ')
-            newDistance = float(input('Enter new distance for ' + toEdit + ': '))
-            r_neighbours[toEdit].linkCost = newDistance
+            newDistance = float(input('Enter new dis for ' + toEdit + ': '))
+            router_neighbours[toEdit].linkCost = newDistance
 
             sendSocket = socket(AF_INET, SOCK_DGRAM)
-            lock.acquire()
-            sendSocket.sendto(create_pkt(toEdit, True), ('localhost', r_neighbours[toEdit].port))
-            lock.release()
+            lockthread.acquire()
+            sendSocket.sendto(pkt_send_creation(toEdit, True), ('localhost', router_neighbours[toEdit].port))
+            lockthread.release()
             sendSocket.close()
 
-            threading.Thread(target=bellManFord).start()
+            threading.Thread(target=distancevectoralgorithm).start()
 
         elif option == 4:
             os._exit(-1)
+        else :
+            continue;
 
 if __name__ == '__main__':  #starting point
     try:
-        r_ID = sys.argv[1]
-        r_port = int(sys.argv[2])
-        r_fileName = sys.argv[3]
+        router_id = sys.argv[1]
+        router_port = int(sys.argv[2])
+        router_filename = sys.argv[3]
     except ValueError or IndexError:
-        print('Incorrect command-line arguments.\nDVR.py <ID> <port> <filename>')
+        print('Incorrect command-line arguments.\npython DVR.py <ID> <port> <filename>')
         exit(0)
 
-    print("Router "+r_ID)
+    print("Router "+router_id)
 
-    r_routes[r_ID] = Path(0, 'direct')
+    router_routes[router_id] = Path(0, 'direct')
 
-    file = open(r_fileName)
+    file = open(router_filename)
     lines = file.readlines()
     for i in range(1, len(lines)):
         tokens = lines[i].split()
-        r_neighbours[tokens[0]] = Neighbour(float(tokens[1]), int(tokens[2]), -1)
-        r_routes[tokens[0]] = Path(float(tokens[1]), 'direct')
-    for id, neighbour in r_neighbours.items():  #new nodes wala kaam
+        router_neighbours[tokens[0]] = Neighbour(float(tokens[1]), int(tokens[2]), -1)
+        router_routes[tokens[0]] = Path(float(tokens[1]), 'direct')
+    for id, neighbour in router_neighbours.items():  #new nodes wala kaam
         p = Path(math.inf, 'direct')
-        for id2, neighbour2 in r_neighbours.items():
+        for id2, neighbour2 in router_neighbours.items():
             neighbour.paths[id2] = p
-        neighbour.paths[r_ID] = Path(0, 'direct')
+        neighbour.paths[router_id] = Path(0, 'direct')
 
-    threading.Thread(target=sendDV, kwargs={'sendLinkCost': True}).start()  #temporary thread for seding DV
+    threading.Thread(target=distancevectorshare, kwargs={'cost': True}).start()  #temporary thread for seding DV
 
-    threading.Thread(target=listen).start()
-    threading.Thread(target=menu).start()
+    threading.Thread(target=threadlisten).start()
+    threading.Thread(target=UserPrompt).start()
     #time.sleep(5)
-    threading.Thread(target=timeOutCheck).start()
+    threading.Thread(target=checktimeout).start()
